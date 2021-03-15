b8de3735031b6ba88aa2af9be49667d68ecb8dfb
Derek-94 <ghdtjrrl94@gmail.com>
2021-03-15 11:17:43 +0900

    feat(problem): Update 1 solution code.
    - Used ascii code comparation
    - Used sort key function and anonymous function.

---
+++ Programmers/Level2/biggest_numeber.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    answer = '';
+    
+    numbers_list = list(map(str, numbers));
+    numbers_list.sort(key = lambda x: x*3, reverse = True);
+    
+    return str(int("".join(numbers_list)))
