9fd54f268561d569ecaf5dc633593aa914c50992
Derek-94 <ghdtjrrl94@gmail.com>
2021-03-23 11:44:17 +0900

    feat(problem): Update 1 solution code.
    - Used sort function.

---
+++ BOJ/class2/10814.py
@@ -0,0 +1,12 @@
+import sys;
+
+test_case = int(sys.stdin.readline());
+arr = [];
+for i in range(test_case):
+    age, name = sys.stdin.readline().split();
+    arr.append((age, name));
+
+arr.sort(key = lambda x: (x[0]));
+
+for i in arr:
+    print(f'{i[0]} {i[1]}')
