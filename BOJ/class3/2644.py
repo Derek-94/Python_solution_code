7e862cf0a70ca9dd6f447a74b70d72a206ca4e0b
Derek-94 <ghdtjrrl94@gmail.com>
2021-04-09 11:00:36 +0900

    feat(problem): Update 1 solution code.
    - Used deque, BFS algorithm.

--- BOJ/class3/2644.py
+++ BOJ/class3/2644.py
@@ -1,29 +1,28 @@
-import sys;
-from collections import deque;
-cin = sys.stdin.readline;
-n = int(cin());
-graph = [[0] * (n + 1) for i in range(n + 1)];
-p1, p2 = map(int, cin().split());
-relation_cnt = int(cin());
-visited = [0] * n;
-
-def bfs(start, goal):
-    depth = 0;
-    que = deque();
-    que.append(start);
-    while que:
-        depth += 1;
-        target = que.popleft();
-        for i in range(n):
-            if visited[i] == 0 and graph[i][target] != 0:
-                que.append(i);
-                visited[i] = 1;
-                if i == goal:
-                    return depth;
-
-for _ in range(relation_cnt):
-    x, y = map(int, cin().split());
-    graph[x][y] = 1;
-    graph[y][x] = 1;
-
-print(min(bfs(p1, p2), bfs(p2, p1)));
+n = int(input())
+a, b = map(int, input().split())
+m = int(input())
+ 
+arr = list([0]*(n) for i in range(n))
+v = list([0]*(n))
+ 
+def bfs(x) : 
+    
+    que = [x]
+ 
+    while que : 
+                
+        cur = que.pop(0)
+               
+        for i in range(n) :
+ 
+            if arr[cur][i] == 1 and v[i] == 0 :                
+                v[i] = v[cur] + 1
+                que.append(i)                   
+    
+for i in range(m) :
+    x, y = map(int, input().split())
+    arr[x-1][y-1] = arr[y-1][x-1] = 1
+ 
+bfs(a-1)
+ 
+print(v[b-1] if v[b-1] else -1)
