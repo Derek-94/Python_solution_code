503852679b3e9b15c62ad295d1b94c33c893d9ec
Derek-94 <ghdtjrrl94@gmail.com>
2021-05-05 18:14:54 +0900

    feat(solution): Update 1 solution code

--- Programmers/Level1/lotto_max_min.py
+++ Programmers/Level1/lotto_max_min.py
@@ -17,9 +17,14 @@ def solution(lottos, win_nums):
     cnt = 0;
     
     for i in range(len(lottos)):
+        # print(f'{lottos[i]}이 {win_nums}에 있나요? {lottos[i] in win_nums}')
         if lottos[i] in win_nums:
+            # print("@@여기다", lottos, lottos[i])
+            #lottos.remove(lottos[i]);
             win_nums.remove(lottos[i]);
+            # print(f'조정후 lotto는 {lottos} 당첨번호는 {win_nums}')
             cnt += 1;
+    # print(cnt)
     answer[1] = place(cnt);
     
     for i in lottos:
