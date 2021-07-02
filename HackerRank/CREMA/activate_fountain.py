0ff553220e6ca8e4ff25a6b3d644ce9a36784c56
Derek-94 <ghdtjrrl94@gmail.com>
2021-07-03 00:39:02 +0900

    feat(problem): Update CREMA code.

--- HackerRank/CREMA/activate_fountain.py
+++ HackerRank/CREMA/activate_fountain.py
@@ -9,49 +9,55 @@ import sys
 
 
 #
-# Complete the 'bitwiseEquations' function below.
+# Complete the 'fountainActivation' function below.
 #
-# The function is expected to return a LONG_INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. LONG_INTEGER_ARRAY a
-#  2. LONG_INTEGER_ARRAY b
-def calculate(target_sum, target_xor):
-    x = (target_sum - target_xor) // 2; # 
-    y = target_sum - x;
-    if bin(target_xor) == bin(x ^ y):
-        return x, y;
-    else:
-        return 0, 0;
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY locations as parameter.
+#
 
-def bitwiseEquations(a, b):
-    # Write your code here
-    results = [];
-    for i in range(len(a)):
-        x, y = calculate(a[i], b[i]);
-        results.append(2 * x + 3 * y);
-    return results
+def fountainActivation(locations):
+    # Answer.
+    fountain_cnt = 0;
+    # Initialize dp array.
+    dp = [-1] * len(locations);
+ 
+    # Set all dp value to its maximum index.
+    for i in range(len(locations)):
+        min_index = max(i - locations[i], 0)
+        max_index = min(i + (locations[i] + 1), len(locations))
+        dp[min_index] = max(dp[min_index],max_index)
+    
+    # Indicies that will be turned on.
+    switch_on_index = 0;
+    # Save first end index.
+    each_end_index = dp[0];
+    
+    for i in range(len(dp)):
+        # Next swich on index is dp[i].
+        if switch_on_index < dp[i]:
+            switch_on_index = dp[i];
+            
+        # i became end index. -> increase fountain cnt.
+        if i == each_end_index:
+            fountain_cnt += 1;
+            each_end_index = switch_on_index;
+        
+    return fountain_cnt + 1; # Add initial one.
+    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    a_count = int(input().strip())
+    locations_count = int(input().strip())
 
-    a = []
+    locations = []
 
-    for _ in range(a_count):
-        a_item = int(input().strip())
-        a.append(a_item)
+    for _ in range(locations_count):
+        locations_item = int(input().strip())
+        locations.append(locations_item)
 
-    b_count = int(input().strip())
+    result = fountainActivation(locations)
 
-    b = []
-
-    for _ in range(b_count):
-        b_item = int(input().strip())
-        b.append(b_item)
-
-    result = bitwiseEquations(a, b)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
+    fptr.write(str(result) + '\n')
 
     fptr.close()
