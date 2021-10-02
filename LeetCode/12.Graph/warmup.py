f3eb7c592130bdd6591d0fe2edbfa9f4e405ba40
Derek-94 <ghdtjrrl94@gmail.com>
2021-10-02 17:07:27 +0900

    feat(problem): Start grapth algorithm.

---
+++ LeetCode/12.Graph/warmup.py
@@ -0,0 +1,33 @@
+graph = {
+    1: [2,3,4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3]
+}
+
+def recursiveBFS(v, visited=[]):
+    visited.append(v);
+    for w in graph[v]:
+        if w not in visited:
+            visited = recursive_dfs(w, visited);
+
+    return visited;
+
+def stackBFS(v):
+    stack = [];
+    visited = [];
+    stack.append(v);
+
+    while stack:
+        nextV = stack.pop();
+        if nextV not in visited:
+            visited.append(nextV);
+            for others in graph[nextV]:
+                stack.append(others);
+    return visited;
+
+print(recursiveBFS(1));
+print(stackBFS(1));
