bc6fba47d921279502fa3e021766d3a785073547
Derek-94 <ghdtjrrl94@gmail.com>
2021-10-06 14:25:50 +0900

    feat(problem): Update bfs solution code.

--- LeetCode/12.Graph/200numberOfIslands.py
+++ LeetCode/12.Graph/200numberOfIslands.py
@@ -1,23 +1,19 @@
 import collections;
 
 class Solution:
+    def BFS(self, deq, dirs, limit_y, limit_x, grid , visited, answer):
+        while len(deq):
+            y, x = deq.popleft();
+            for dir in dirs:
+                ny = y + dir[0];
+                nx = x + dir[1];
+                if 0 <= nx < limit_x and 0 <= ny < limit_y and visited[ny][nx] == 0 and grid[ny][nx] == '1':
+                    deq.append((ny, nx));
+                    visited[ny][nx] = 1;
     
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-        
-        def BFS(deq, limit_y, limit_x, grid , visited, answer):
-            while len(deq):
-                y, x = deq.popleft();
-                for dir in dirs:
-                    ny = y + dir[0];
-                    nx = x + dir[1];
-                    if 0 <= nx < limit_x and 0 <= ny < limit_y and visited[ny][nx] == 0 and grid[ny][nx] == '1':
-                        deq.append((ny, nx));
-                        visited[ny][nx] = 1;
-        
         answer = 0;
-        
         deq = collections.deque();
         
         visited = [[0] * len(grid[0]) for _ in range(len(grid))];
@@ -27,7 +23,7 @@ class Solution:
                 if grid[y][x] == '1' and visited[y][x] == 0:
                     deq.append((y, x));
                     visited[y][x] = 1;
-                    BFS(deq, len(grid), len(grid[0]), grid, visited, answer);
+                    self.BFS(deq, dirs, len(grid), len(grid[0]), grid, visited, answer);
                     answer += 1;
                     
         return answer;
