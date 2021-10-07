23c56ac624bfda8c5cb85cf20f93f96d0909849b
Derek-94 <ghdtjrrl94@gmail.com>
2021-10-07 20:24:56 +0900

    feat(problem): Update 1 solution code.

--- LeetCode/15.heap/215kthLargestElement.py
+++ LeetCode/15.heap/215kthLargestElement.py
@@ -1,3 +1,15 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return sorted(nums, reverse=True)[k - 1];
+
+
+class Solution1:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        import heapq;
+        
+        heapq.heapify(nums);
+        
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums);
+        
+        return heapq.heappop(nums)
